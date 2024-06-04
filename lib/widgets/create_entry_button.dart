3ac7854d40b8f99b03b471be61c7453ddb3d5aa4
jpgtzg <58833781+jpgtzg@users.noreply.github.com>
2024-06-03 21:37:22 -0600

    Added create_blog button and modified entry button

--- lib/widgets/create_entry_button.dart
+++ lib/widgets/create_entry_button.dart
@@ -3,80 +3,19 @@
 
 import 'package:flutter/material.dart';
 import 'package:self_blog/constants.dart';
-import 'package:self_blog/system/realm_manager.dart';
-import 'package:self_blog/system/realm_models.dart';
 
 class CreateEntryButton extends StatelessWidget {
-  final String titleText;
-  final Color primaryColor;
-  final Color? secondaryColor;
-  final Color buttonColor;
-  final Note note;
   final Function onTap;
-  const CreateEntryButton(
-      {required this.titleText,
-      required this.primaryColor,
-      this.secondaryColor,
-      required this.buttonColor,
-      required this.note,
-      required this.onTap,
-      super.key});
+  const CreateEntryButton({required this.onTap, super.key});
 
   @override
   Widget build(BuildContext context) {
-    return GestureDetector(
-      onTap: () {
-        onTap();
-      },
-      child: AspectRatio(
-        aspectRatio: 13 / 4,
-        child: Container(
-          padding: const EdgeInsets.only(
-            top: 15,
-            bottom: 25,
-            left: 25,
-          ),
-          decoration: (secondaryColor != null)
-              ? BoxDecoration(
-                  borderRadius: BorderRadius.circular(25),
-                  gradient: LinearGradient(
-                    begin: Alignment.topRight,
-                    end: Alignment.bottomLeft,
-                    colors: [
-                      primaryColor,
-                      secondaryColor!,
-                    ],
-                  ),
-                )
-              : BoxDecoration(
-                  color: primaryColor,
-                  borderRadius: BorderRadius.circular(25),
-                ),
-          child: Stack(
-            children: [
-              Text(
-                titleText,
-                textAlign: TextAlign.center,
-                style: defaultStyle,
-              ),
-              Positioned(
-                right: 30,
-                bottom: 0,
-                child: Container(
-                  padding: const EdgeInsets.all(2),
-                  height: 40,
-                  width: 120,
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(8),
-                    color: buttonColor,
-                  ),
-                  child: const Center(
-                      child: Icon(Icons.add, color: Colors.white, size: 30)),
-                ),
-              ),
-            ],
-          ),
-        ),
+    return Align(
+      alignment: Alignment.bottomRight,
+      child: FloatingActionButton(
+        backgroundColor: paleteLightBlue.withAlpha(100),
+        child: const Icon(Icons.add, color: Colors.white, size: 30),
+        onPressed: () => onTap(),
       ),
     );
   }
