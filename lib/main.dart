f9ed208dc714b6a69f0b09c139b2bff1141d469d
jpgtzg <58833781+jpgtzg@users.noreply.github.com>
2024-06-03 21:43:02 -0600

    Cleaned up main

--- lib/main.dart
+++ lib/main.dart
@@ -10,14 +10,6 @@ import 'package:self_blog/system/realm_models.dart';
 void main() {
   initRealm();
 
-  Note note = Note("3 / 06 / 2025");
-  Entry firstEntry = Entry("11:00", "Sad", "Im felling sad today");
-  note.entries.add(firstEntry);
-
-  // write(note);
-
-  // print(getNotes().first.date);
-
   runApp(const MainApp());
 }
 
