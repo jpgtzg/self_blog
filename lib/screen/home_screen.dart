a76d3e3a57368cfb3d4ed35c470fb82b81ee0447
jpgtzg <58833781+jpgtzg@users.noreply.github.com>
2024-06-03 19:53:20 -0600

    Finished home_screen

--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -3,21 +3,47 @@
 
 import 'package:flutter/material.dart';
 import 'package:self_blog/constants.dart';
+import 'package:self_blog/screen/note_screen.dart';
+import 'package:self_blog/system/realm_manager.dart';
 import 'package:self_blog/widgets/gradient_scaffold.dart';
+import 'package:self_blog/widgets/note_card.dart';
+import 'package:self_blog/widgets/standard_spacer.dart';
 import 'package:self_blog/widgets/top_menu_bar.dart';
 
 class HomeScreen extends StatelessWidget {
-  const HomeScreen({super.key});
+  HomeScreen({super.key});
+
+  final notes = getNotes();
 
   @override
   Widget build(BuildContext context) {
-    return const GradientScaffold(
+    return GradientScaffold(
       gradient: backGroundGradient,
       isCentered: true,
       isInSafeArea: true,
+      padding: const EdgeInsets.only(left: 15.0, right: 15.0),
       body: Column(
         children: [
-          TopMenuBar(),
+          const TopMenuBar(),
+          Expanded(
+            child: ListView.builder(
+              itemCount: notes.length,
+              itemBuilder: (context, index) {
+                return Column(
+                  children: [
+                    NoteCard(
+                      titleText: notes[index].date,
+                      widgetScreen: NoteScreen(note: notes[index]),
+                      primaryColor: paleteLightBlue.withAlpha(75),
+                      buttonColor: paleteLightBlue.withAlpha(150),
+                      note: notes[index],
+                    ),
+                    const StandardSpacer(height: standartSpacerHeight),
+                  ],
+                );
+              },
+            ),
+          ),
         ],
       ),
     );
