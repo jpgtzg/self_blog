9d511208a64f0963a64f8788ea539087184ffb65
jpgtzg <58833781+jpgtzg@users.noreply.github.com>
2024-07-09 21:18:15 -0600

    Created theme_Screen

---
+++ lib/screen/theme_screen.dart
@@ -0,0 +1,18 @@
+// Written by  Juan Pablo Gutiérrez
+// 09 07 2024
+
+import 'package:flutter/material.dart';
+
+class ThemeScreen extends StatefulWidget {
+  const ThemeScreen({super.key});
+
+  @override
+  State<ThemeScreen> createState() => _ThemeScreenState();
+}
+
+class _ThemeScreenState extends State<ThemeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
