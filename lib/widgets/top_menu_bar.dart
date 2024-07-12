a0318dd2b9eb98ce89bacefcb7faffb808beb541
jpgtzg <58833781+jpgtzg@users.noreply.github.com>
2024-07-11 23:04:21 -0600

    Changed TopMenuBar to allow for Menu button click

--- lib/widgets/top_menu_bar.dart
+++ lib/widgets/top_menu_bar.dart
@@ -5,29 +5,33 @@ import 'package:flutter/material.dart';
 import 'package:self_blog/widgets/big_text.dart';
 
 class TopMenuBar extends StatelessWidget {
-  const TopMenuBar({super.key});
+  final Function onMenuPressed;
+
+  const TopMenuBar({required this.onMenuPressed, super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Padding(
-      padding: EdgeInsets.only(left: 12.5, right: 12.5),
+    return Padding(
+      padding: const EdgeInsets.only(left: 12.5, right: 12.5),
       child: Column(
         children: [
-          SizedBox(
+          const SizedBox(
             height: 20,
           ),
           Row(
             mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: [
-              BigText(titleText: "Welcome back", textAlign: TextAlign.center),
-              Icon(
-                Icons.menu,
-                color: Colors.white,
-                size: 30,
-              )
+              const BigText(titleText: "Welcome back", textAlign: TextAlign.center),
+              IconButton(
+                  onPressed: () => onMenuPressed.call(),
+                  icon: const Icon(
+                    Icons.menu,
+                    color: Colors.white,
+                    size: 30,
+                  )),
             ],
           ),
-          SizedBox(
+          const SizedBox(
             height: 5,
           ),
         ],
