9b704df76e5949ff722385fe8b4ab1d6eaa63f6f
jpgtzg <58833781+jpgtzg@users.noreply.github.com>
2024-07-21 23:18:47 -0600

    Added scaffold to theme screen

--- lib/screen/theme_screen.dart
+++ lib/screen/theme_screen.dart
@@ -2,6 +2,10 @@
 // 09 07 2024
 
 import 'package:flutter/material.dart';
+import 'package:self_blog/constants.dart';
+import 'package:self_blog/widgets/back_bar.dart';
+import 'package:self_blog/widgets/gradient_scaffold.dart';
+import 'package:self_blog/widgets/standard_spacer.dart';
 
 class ThemeScreen extends StatefulWidget {
   const ThemeScreen({super.key});
@@ -13,6 +17,20 @@ class ThemeScreen extends StatefulWidget {
 class _ThemeScreenState extends State<ThemeScreen> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return const GradientScaffold(
+      gradient: backGroundGradient,
+      isCentered: true,
+      isInSafeArea: true,
+      padding: EdgeInsets.only(left: 15.0, right: 15.0),
+      body: Column(
+        children: [
+          BackBar(),
+          StandardSpacer(
+            value: standardSpacerHeight,
+            direction: Direction.vertical,
+          ),
+        ],
+      ),
+    );
   }
 }
